06319fd8c28ec484f261840c40a10c65907efcfb
Ganugula Vinaya <104989608+Ganugula-Vinaya@users.noreply.github.com>
2025-07-28 16:49:38 +0530

    Add files via upload

---
+++ Calculator.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorAppUsingMSTest
+{
+    public class Calculator
+    {
+        public int Multiply(int a, int b)
+        {
+            return a * b;
+        }
+
+    }
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+
+}
---
+++ Test1.cs
@@ -0,0 +1,27 @@
+﻿//Add 
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CalculatorAppUsingMSTest;
+
+
+namespace MathsLibrary.MSTest
+{
+    [TestClass]
+    public sealed class Test1
+    {
+        private Calculator calculator;//ref of a class
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            calculator = new Calculator();
+        }
+
+        [TestMethod]
+        public void Add_ShouldReturnsMultiply()
+        {
+            int result = calculator.Multiply(2, 3);
+            Assert.AreEqual(6, result);
+            //assert is used to verify the result of the operation
+        }
+       
+    }
+}
